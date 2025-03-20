f58bf3970766dae6337abb21f069ef876ee7de9e
KarinaSkirmante <karina.krinkele@gmail.com>
2025-03-20 16:05:46 +0200

    Edit: added variables, contructor, isEmpty, isFull, howManyElements, resize

--- src/datastr/MyGraph.java
+++ src/datastr/MyGraph.java
@@ -1,5 +1,65 @@
 package datastr;
 
-public class MyGraph {
+public class MyGraph <Ttype> {
 
+	private Ttype[] vertices;
+	private int[][] edges;
+	private final int DEFAULT_SIZE = 10;
+	private int size = DEFAULT_SIZE;
+	private int counter = 0;
+	
+	public MyGraph()
+	{
+		vertices = (Ttype[])new Object[size];
+		edges = new int[size][size];
+	}
+	
+	public MyGraph(int inputSize) {
+		if(inputSize > 0) {
+			size = inputSize;
+		}
+		vertices = (Ttype[])new Object[size];
+		edges = new int[size][size];
+	}
+	public boolean isFull()
+	{
+		return (counter == size);	
+	}
+	
+	public boolean isEmpty() {
+		return (counter == 0);
+	}
+	
+	public int howManyElements() {
+		return counter;
+	}
+	
+	
+	private void resize()
+	{
+		size = (counter < 100) ? size * 2 : (int)(size * 1.5);
+		Ttype[] newVertices = (Ttype[])new Object[size];
+		int[][] newEdges = new int[size][size];
+		
+		for(int i = 0; i < counter; i++) {
+			newVertices[i] = vertices[i];
+		}
+		
+		for(int i = 0; i < counter; i++) {
+			for(int j = 0; j < counter; j++) {
+				newEdges[i][j] = edges[i][j];
+			}
+		}
+		
+		vertices = newVertices;
+		edges = newEdges;
+		System.gc();
+		
+	}
+
+	
+	//addVertice
+	//addEdge
+	//print
+	//makeEmpty
 }
